16edb6f04edc5c0bc486f20f69b38d5fa45a824d
xStrikea <coder4819@gmail.com>
2025-09-07 08:30:32 +0800

    update main.lua

---
+++ scripthub/main.lua
@@ -0,0 +1,31 @@
+if jumpscare_jeffwuz_loaded and not _G.jumpscarefucking123 == true then
+warn("Already Loading")
+return
+end
+
+pcall(function() getgenv().jumpscare_jeffwuz_loaded = true end)
+
+if not getcustomasset then
+game:Shutdown() 
+end
+
+local player = game:GetService("Players").LocalPlayer
+local HttpService = game:GetService('HttpService')
+
+local ScreenGui = Instance.new("ScreenGui")
+local VideoScreen = Instance.new("VideoFrame")
+ScreenGui.Parent = game:GetService("CoreGui")
+ScreenGui.IgnoreGuiInset = true
+ScreenGui.Name = "JeffTheKillerWuzHere"
+
+VideoScreen.Parent = ScreenGui
+VideoScreen.Size = UDim2.new(1,0,1,0)
+
+writefile("face.mp4", game:HttpGet("https://xstrikea.github.io/scripthub/face.mp4"))
+
+VideoScreen.Video = getcustomasset("face.mp4")
+
+VideoScreen.Looped = true
+VideoScreen.Playing = true
+VideoScreen.Volume = 10
+
